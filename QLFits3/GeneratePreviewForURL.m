92ea1858bf39ced1e3a72efe76ac04780d80887b
Cédric Foellmi <cedric@onekilopars.ec>
2014-01-02 09:51:34 +0100

    Made the saving of images into /tmp and fixed various HTML UI stuff

--- QLFits3/GeneratePreviewForURL.m
+++ QLFits3/GeneratePreviewForURL.m
@@ -6,6 +6,8 @@
 #import "Common.h"
 #import "cdlzscale.h"
 
+#define DRAW_IMAGE_IN_QL_CONTEXT 0
+
 OSStatus GeneratePreviewForURL(void *thisInterface,
 							   QLPreviewRequestRef preview,
 							   CFURLRef url,
@@ -25,128 +27,135 @@ OSStatus GeneratePreviewForURL(void *thisInterface,
     DebugLog(@"Previewing %@", (__bridge NSURL *)url);
 
 	@autoreleasepool {
+		BOOL success = NO;
 
-		CGSize maxSize = CGSizeMake(800, 800);
+		NSString *synthesizedValue = nil;
+		NSMutableDictionary *synthesizedInfo = [NSMutableDictionary dictionary];
+		[synthesizedInfo setObject:[[(__bridge NSURL *)url absoluteString] lastPathComponent] forKey:@"FileName"];
+
+		NSDictionary *shortSummary = [FITSFile FITSFileShortSummaryWithURL:(__bridge NSURL *)url];
+		[synthesizedInfo setObject:(shortSummary) ? shortSummary[@"summary"] : @"" forKey:@"ContentSummary"];
 
 		FITSFile *fits = [FITSFile FITSFileWithURL:(__bridge NSURL *)url];
 		[fits open];
 
-		if ([fits countOfHDUs] >= 1 && !QLPreviewRequestIsCancelled(preview)) {
-			[fits syncLoadHeaderOfHDUAtIndex:0];
+		if ([fits countOfHDUs] == 0 || QLPreviewRequestIsCancelled(preview)) {
+			[fits close];
+		}
+		else {
+			success = [fits syncLoadHeaderOfMainHDU];
 
-/*
-			NSURL *htmlURL = [[NSBundle bundleWithIdentifier:@"com.iconfactory.Provisioning"] URLForResource:@"template" withExtension:@"html"];
-			NSMutableString *html = [NSMutableString stringWithContentsOfURL:htmlURL encoding:NSUTF8StringEncoding error:NULL];
+			NSMutableString *headerString = [NSMutableString string];
+			[headerString appendString:@"<table>\n"];
 
-			NSMutableDictionary *synthesizedInfo = [NSMutableDictionary dictionary];
-			NSString *synthesizedValue = nil;
+			if (success) {
+				FITSHeader *header = [[fits mainHDU] header];
 
-			// Do all the work here
+				synthesizedValue = [NSString stringWithFormat:@"(%zd line%s)", [header countOfLines], ([header countOfLines] == 1 ? "" : "s")];
+				[synthesizedInfo setObject:synthesizedValue forKey:@"HeaderLinesCount"];
 
-			// Header
-
-			NSArray *array = (NSArray *)value;
-			NSArray *sortedArray = [array sortedArrayUsingSelector:@selector(compare:)];
-
-			NSMutableString *devices = [NSMutableString string];
-			[devices appendString:@"<table>\n"];
-			BOOL evenRow = NO;
-			for (NSString *device in sortedArray) {
-				// compute the prefix for the first column of the table
-				NSString *displayPrefix = @"";
-				NSString *devicePrefix = [device substringToIndex:1];
-				if (! [currentPrefix isEqualToString:devicePrefix]) {
-					currentPrefix = devicePrefix;
-					displayPrefix = [NSString stringWithFormat:@"%@ ➞ ", devicePrefix];
+				for (NSUInteger i = 0; i < [header countOfLines]; i++) {
+					FITSHeaderLine *headerLine = [header lineAtIndex:i];
+					[headerString appendFormat:@"<tr><td class=FITSHeaderKey>%@</td><td> = </td>", headerLine.key];
+					[headerString appendFormat:@"<td class=FITSHeaderValue>%@</td><td> / </td>", headerLine.value];
+					[headerString appendFormat:@"<td class=FITSHeaderComment>%@</td></tr>", headerLine.comment];
 				}
-
-#if !SIGNED_CODE
-				// check if Xcode has seen the device
-				NSString *deviceName = @"";
-				NSString *deviceSoftwareVerson = @"";
-				NSPredicate *predicate = [NSPredicate predicateWithFormat:@"deviceIdentifier = %@", device];
-				NSArray *matchingDevices = [savedDevices filteredArrayUsingPredicate:predicate];
-				if ([matchingDevices count] > 0) {
-					id matchingDevice = [matchingDevices firstObject];
-					if ([matchingDevice isKindOfClass:[NSDictionary class]]) {
-						NSDictionary *matchingDeviceDictionary = (NSDictionary *)matchingDevice;
-						deviceName = [matchingDeviceDictionary objectForKey:@"deviceName"];
-						deviceSoftwareVerson = [matchingDeviceDictionary objectForKey:@"deviceSoftwareVersion"];
-					}
-				}
-
-				[devices appendFormat:@"<tr class='%s'><td>%@</td><td>%@</td><td>%@</td><td>%@</td></tr>\n", (evenRow ? "even" : "odd"), displayPrefix, device, deviceName, deviceSoftwareVerson];
-#else
-				[devices appendFormat:@"<tr class='%s'><td>%@</td><td>%@</td></tr>\n", (evenRow ? "even" : "odd"), displayPrefix, device];
-#endif
-				evenRow = !evenRow;
 			}
-			[devices appendString:@"</table>\n"];
 
-			synthesizedValue = [devices copy];
+			[headerString appendString:@"</table>\n"];
+
+			synthesizedValue = [headerString copy];
 			[synthesizedInfo setObject:synthesizedValue forKey:@"HeaderLinesFormatted"];
 
-			synthesizedValue = [NSString stringWithFormat:@"%zd Device%s", [array count], ([array count] == 1 ? "" : "s")];
-			[synthesizedInfo setObject:synthesizedValue forKey:@"HeaderLinesCount"];
 
-			for (NSString *key in [synthesizedInfo allKeys]) {
-				NSString *replacementValue = [synthesizedInfo objectForKey:key];
-				NSString *replacementToken = [NSString stringWithFormat:@"__%@__", key];
-				[html replaceOccurrencesOfString:replacementToken withString:replacementValue options:0 range:NSMakeRange(0, [html length])];
-			}
+			for (NSUInteger i = 0; i < [fits countOfHDUs]; i++) {
+				success = [fits syncLoadDataOfHDUAtIndex:i];
+				if (success) {
+					FITSHDU *hdu = [fits HDUAtIndex:i];
 
-			NSDictionary *properties = @{ // properties for the HTML data
-										 (__bridge NSString *)kQLPreviewPropertyTextEncodingNameKey : @"UTF-8",
-										 (__bridge NSString *)kQLPreviewPropertyMIMETypeKey : @"text/html" };
+					if ([hdu type] == FITSHDUTypeImage) {
+						NSString *objectName = [[hdu header] stringValueForKey:@"OBJECT"];
+						FITSImage *img = [hdu image];
+						CGSize maxSize = CGSizeMake(800.0, 800.0);
 
-			QLPreviewRequestSetDataRepresentation(preview,
-												  (__bridge CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding],
-												  kUTTypeHTML,
-												  (__bridge CFDictionaryRef)properties);
-*/
-
-
-			[fits syncLoadDataOfHDUAtIndex:0];
-
-			FITSHDU *hdu = [[fits HDUs] objectAtIndex:0];
-			NSString *objectName = [[hdu header] stringValueForKey:@"OBJECT"];
-
-			if ([hdu type] == FITSHDUTypeImage) {
-				FITSImage *img = [hdu image];
-
-				NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
-										 objectName, kQLPreviewPropertyDisplayNameKey,
-										 [NSNumber numberWithDouble:600.0], kQLPreviewPropertyWidthKey,
-										 [NSNumber numberWithDouble:500.0], kQLPreviewPropertyHeightKey,
-										 nil];
-
-				if ([img is2D]) {
-					CGImageRef cgImage = [img CGImageScaledToSize:maxSize];
-					if (cgImage != NULL) {
-						CGContextRef context = QLPreviewRequestCreateContext(preview, maxSize, true, (__bridge CFDictionaryRef)options);
-						CGRect renderRect = CGRectMake(0., 0., maxSize.width, maxSize.height);
-						CGContextDrawImage(context, renderRect, cgImage);
 						if (objectName == nil) objectName = [(__bridge NSURL *)url lastPathComponent];
-						DrawObjectName(context, renderRect.size, objectName);
-						QLPreviewRequestFlushContext(preview, context);
-						CFRelease(context);
+						NSDictionary *options = @{(__bridge NSString *)kQLPreviewPropertyDisplayNameKey: objectName,
+												  (__bridge NSNumber *)kQLPreviewPropertyWidthKey: @(800),
+												  (__bridge NSNumber *)kQLPreviewPropertyHeightKey: @(800)};
+
+						// We use "NSUserName" to avoid collisions for plugins installed on whole system in multi-users machines.
+						NSString *HDUImageFileName = [NSString stringWithFormat:@"/tmp/QLFits3_%@_ext%lu.tiff", NSUserName(), i+1];
+
+						if ([img is2D]) {
+							CGImageRef cgImage = [img CGImageScaledToSize:maxSize];
+							if (cgImage != NULL) {
+								if (DRAW_IMAGE_IN_QL_CONTEXT) {
+									CGContextRef context = QLPreviewRequestCreateContext(preview, maxSize, true, (__bridge CFDictionaryRef)options);
+									CGRect renderRect = CGRectMake(0., 0., maxSize.width, maxSize.height);
+									CGContextDrawImage(context, renderRect, cgImage);
+									DrawObjectName(context, renderRect.size, objectName);
+									QLPreviewRequestFlushContext(preview, context);
+									CFRelease(context);
+								}
+								else {
+									NSImage *img = [[NSImage alloc] initWithCGImage:cgImage size:maxSize];
+									success = [[img TIFFRepresentation] writeToFile:HDUImageFileName atomically:YES];
+								}
+							}
+						}
+						else if ([img is1D]) {
+							FITSSpectrum *spectrum = [img spectrum];
+							if (spectrum != nil) {
+								if (DRAW_IMAGE_IN_QL_CONTEXT) {
+									CGContextRef context = QLPreviewRequestCreateContext(preview, maxSize, true, (__bridge CFDictionaryRef)options);
+									DrawSpectrumCanvas(context, maxSize);
+									DrawSpectrum(context, maxSize, spectrum);
+									DrawObjectName(context, maxSize, objectName);
+									CGContextFlush(context);
+									QLPreviewRequestFlushContext(preview, context);
+									CFRelease(context);
+								}
+								else {
+									NSImage *img = [[NSImage alloc] initWithSize:maxSize];
+									[img lockFocus];
+									CGContextRef context = [[NSGraphicsContext currentContext] graphicsPort];
+									DrawSpectrumCanvas(context, maxSize);
+									DrawSpectrum(context, maxSize, spectrum);
+									DrawObjectName(context, maxSize, objectName);
+									CGContextFlush(context);
+									[img unlockFocus];
+									success = [[img TIFFRepresentation] writeToFile:HDUImageFileName atomically:YES];
+								}
+							}
+						}
 					}
-				}
-				else if ([img is1D]) {
-					FITSSpectrum *spectrum = [img spectrum];
-
-					CGContextRef context = QLPreviewRequestCreateContext(preview, maxSize, true, (__bridge CFDictionaryRef)options);
-					DrawSpectrumCanvas(context, maxSize);
-					DrawSpectrum(context, maxSize, spectrum);
-					if (objectName == nil) objectName = [(__bridge NSURL *)url lastPathComponent];
-					DrawObjectName(context, maxSize, objectName);
-					CGContextFlush(context);
-
-					QLPreviewRequestFlushContext(preview, context);
-					CFRelease(context);
 				}
 			}
 		}
+
+		NSBundle *bundle = [NSBundle bundleWithIdentifier:@"com.onekiloparsec.QLFits3"];
+		NSString *versionString = [[bundle infoDictionary] objectForKey:@"CFBundleVersion"];
+		[synthesizedInfo setObject:versionString forKey:@"BundleVersion"];
+
+		NSURL *htmlURL = [bundle URLForResource:@"template" withExtension:@"html"];
+		NSMutableString *html = [NSMutableString stringWithContentsOfURL:htmlURL encoding:NSUTF8StringEncoding error:NULL];
+
+		for (NSString *key in [synthesizedInfo allKeys]) {
+			NSString *replacementValue = [synthesizedInfo objectForKey:key];
+			NSString *replacementToken = [NSString stringWithFormat:@"__%@__", key];
+			[html replaceOccurrencesOfString:replacementToken withString:replacementValue options:0 range:NSMakeRange(0, [html length])];
+		}
+
+		NSDictionary *properties = @{(__bridge NSString *)kQLPreviewPropertyTextEncodingNameKey : @"UTF-8",
+									 (__bridge NSString *)kQLPreviewPropertyMIMETypeKey : @"text/html",
+									 (__bridge NSNumber *)kQLPreviewPropertyWidthKey : @(800),
+									 (__bridge NSNumber *)kQLPreviewPropertyHeightKey : @(800) };
+
+		QLPreviewRequestSetDataRepresentation(preview,
+											  (__bridge CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding],
+											  kUTTypeHTML,
+											  (__bridge CFDictionaryRef)properties);
+
 	}
 
     return noErr;
