da2e1d0e69459664ce82039cd5515142d567eb1a
Cédric Foellmi <cedric@onekilopars.ec>
2014-11-02 09:51:51 +0100

    Removing pods as part of the git repo

---
+++ QLFits3/NSData+gzip.h
@@ -0,0 +1,13 @@
+//
+//  NSData+gzip.h
+//  QLFits3
+//
+//  Created by Cédric Foellmi on 31/10/14.
+//  Copyright (c) 2014 onekiloparsec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (gzip)
+
+@end
---
+++ QLFits3/NSData+gzip.m
@@ -0,0 +1,13 @@
+//
+//  NSData+gzip.m
+//  QLFits3
+//
+//  Created by Cédric Foellmi on 31/10/14.
+//  Copyright (c) 2014 onekiloparsec. All rights reserved.
+//
+
+#import "NSData+gzip.h"
+
+@implementation NSData (gzip)
+
+@end
